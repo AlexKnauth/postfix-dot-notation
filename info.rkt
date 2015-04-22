6a9aa939619294ac4ede5272b684f8995fffb5f7
AlexKnauth <alexander@knauth.org>
2015-04-22 17:04:10 -0400

    update deps

--- info.rkt
+++ info.rkt
@@ -4,5 +4,5 @@
 
 (define deps '("base" "sweet-exp"))
 
-(define build-deps '("rackunit-lib" "scribble-lib" "racket-doc"))
+(define build-deps '("rackunit-lib" "scribble-lib" "racket-doc" "scribble-doc"))
 
